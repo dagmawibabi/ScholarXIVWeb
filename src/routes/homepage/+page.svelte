cb6e5a7a0a3ca6d1c041112d725fdaad4adce4b7
dagmawibabi <1babidagi@gmail.com>
2025-02-11 03:13:21 +0300

    works on mobile too

--- src/routes/homepage/+page.svelte
+++ src/routes/homepage/+page.svelte
@@ -22,7 +22,7 @@
 	});
 </script>
 
-<div class="m-auto w-full md:w-2/3 lg:w-2/4 xl:w-2/5 2xl:w-2/5">
+<div class="m-auto w-full px-3 md:w-2/3 lg:w-2/4 lg:px-0 xl:w-2/5 xl:px-0 2xl:w-2/5 2xl:px-0">
 	<!-- Title and Profile -->
 	<Navigation />
 

4c224c79c5ba98b4c74cd81f03c9afb52f4e8732
dagmawibabi <1babidagi@gmail.com>
2025-06-29 02:44:58 +0300

    session duration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,6 +7,10 @@ export const auth = betterAuth({
 	emailAndPassword: {
 		enabled: true
 	},
+	session: {
+		expiresIn: 60 * 60 * 24 * 7, // 7 days
+		updateAge: 60 * 60 * 24 // 1 day (every 1 day the session expiration is updated)
+	},
 	socialProviders: {
 		google: {
 			clientId: process.env.GOOGLE_CLIENT_ID as string,

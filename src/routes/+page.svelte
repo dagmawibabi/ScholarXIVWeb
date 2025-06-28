84ef237f5e5252798273fa738d49624b323bb68e
dagmawibabi <1babidagi@gmail.com>
2025-06-29 02:54:37 +0300

    remember session

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import SignIn from './auth/sign_in/+page.svelte';
+	import { authClient } from '$lib/auth_client';
+	import HomePage from './homepage/+page.svelte';
+
+	const session = authClient.useSession();
 </script>
 
 <svelte:head>
@@ -7,5 +11,9 @@
 </svelte:head>
 
 <div class="m-auto w-full px-3 md:w-2/3 lg:w-2/4 lg:px-0 xl:w-2/5 xl:px-0 2xl:w-2/5 2xl:px-0">
-	<SignIn />
+	{#if $session.data}
+		<HomePage />
+	{:else}
+		<SignIn />
+	{/if}
 </div>

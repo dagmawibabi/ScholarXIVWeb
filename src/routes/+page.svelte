56f5e2edc686092086ff3a6291f3089672746ea7
dagmawibabi <1babidagi@gmail.com>
2025-06-29 03:02:53 +0300

    remember session

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,10 +10,10 @@
 	<title>ScholarXIV</title>
 </svelte:head>
 
-<div class="m-auto w-full px-3 md:w-2/3 lg:w-2/4 lg:px-0 xl:w-2/5 xl:px-0 2xl:w-2/5 2xl:px-0">
-	{#if $session.data}
-		<HomePage />
-	{:else}
+{#if $session.data}
+	<HomePage />
+{:else}
+	<div class="m-auto w-full px-3 md:w-2/3 lg:w-2/4 lg:px-0 xl:w-2/5 xl:px-0 2xl:w-2/5 2xl:px-0">
 		<SignIn />
-	{/if}
-</div>
+	</div>
+{/if}
